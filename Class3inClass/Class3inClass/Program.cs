521efdb7c558d895e13c23030d3ac826c22a6769
DavidAJackson <BigDave802@gmail.com>
2016-10-13 23:09:30 -0500

    Homework 3.... yay

---
+++ Class3inClass/Class3inClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class3inClass
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("input number: ");
+            string userinput = Console.ReadLine();
+            int xval = int.Parse(userinput);
+            int xcalc = (int)((5 * xval * xval) + 6);
+            Console.WriteLine("the answer is {0}", xcalc);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ HomeWork3/HomeWork3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    class Program
+    {
+        static void Main()
+        {
+            // all the talking should be done in a Mr. T voice
+
+            Console.Write("Input a number of seconds, fool! Make it a big number, but keep it under 2 billion!: ");
+            string input = Console.ReadLine();
+            int sec = int.Parse(input);
+            int min = sec / 60;
+            int hrs = min / 60;
+            int day = hrs / 24;
+
+            Console.WriteLine("");
+            Console.WriteLine("I pity the fact that you put in {0} seconds!", sec);
+            Console.WriteLine("Can you even count to {0} minutes?", min);
+            Console.WriteLine("That's equal to {0} hours man!", hrs);
+            Console.WriteLine("{0} days is a long time to feel the pain!", day);
+            Console.ReadLine();
+
+           
+        }
+    }
+}
